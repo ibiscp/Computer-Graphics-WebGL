9a51399cc9758bae3371bd3e79d450b94a4868a0
Ibis Prevedello <ibiscp@gmail.com>
2018-04-30 14:20:36 +0200

    refac: Matrices substituted by functions

--- Homework1/Homework1.js
+++ Homework1/Homework1.js
@@ -1,28 +1,74 @@
 "use strict";
 
+// Program
 var canvas;
 var gl;
-
 var numVertices  = 36;
-
-var numChecks = 8;
-
 var program;
 
-var c;
-
+// Buttons
 var flag = true;
-
 var direction = true;
-var perspective = true;
+var perspec = false;
+
+// Sliders
 var sliderTX;
 var sliderTY;
 var sliderTZ;
-var n;
-var f;
+var sliderScale;
 
+// Arrays
 var pointsArray = [];
 var colorsArray = [];
+var normalsArray = [];
+
+// Matrices
+var modelViewMatrix;
+var projectionMatrix;
+
+// Camera
+var eye = vec3(0.0, 0.0, 1.0);
+const at = vec3(0.0, 0.0, 0.0);
+const up = vec3(0.0, 1.0, 0.0);
+
+// Orthogonal
+var left = -1.0;
+var right = 1.0;
+var bottom = -1.0;
+var ytop = 1.0;
+var near;
+var far;
+
+// Perspective
+var fovy = 100.0;
+var aspect = 1.0;
+
+// Axis
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+var axis = xAxis;
+
+// Cube Angle
+var theta = [45.0, 45.0, 45.0];
+
+// Lights
+var ambientColor, diffuseColor, specularColor;
+var lightX;
+var lightY;
+var lightZ;
+
+// Lights
+var lightPosition = vec4(1.0, 1.0, 1.0, 0.0 );
+var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
+var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
+var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+
+// Materials
+var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
+var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0);
+var materialSpecular = vec4( 1.0, 0.8, 0.0, 1.0 );
+var materialShininess = 100.0;
 
 var vertices = [
     vec4( -0.5, -0.5,  0.5, 1.0 ),
@@ -46,33 +92,37 @@ var vertexColors = [
     vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
 ];
 
-var xAxis = 0;
-var yAxis = 1;
-var zAxis = 2;
-var axis = xAxis;
-
-var theta = [45.0, 45.0, 45.0];
-
-var thetaLoc;
-
 function quad(a, b, c, d) {
+
+     var t1 = subtract(vertices[b], vertices[a]);
+     var t2 = subtract(vertices[c], vertices[b]);
+     var normal = cross(t1, t2);
+     var normal = vec3(normal);
+     normal = normalize(normal);
+
      pointsArray.push(vertices[a]);
      colorsArray.push(vertexColors[a]);
+     normalsArray.push(normal);
 
      pointsArray.push(vertices[b]);
      colorsArray.push(vertexColors[a]);
+     normalsArray.push(normal);
 
      pointsArray.push(vertices[c]);
      colorsArray.push(vertexColors[a]);
+     normalsArray.push(normal);
 
      pointsArray.push(vertices[a]);
      colorsArray.push(vertexColors[a]);
+     normalsArray.push(normal);
 
      pointsArray.push(vertices[c]);
      colorsArray.push(vertexColors[a]);
+     normalsArray.push(normal);
 
      pointsArray.push(vertices[d]);
      colorsArray.push(vertexColors[a]);
+     normalsArray.push(normal);
 }
 
 function colorCube()
@@ -84,7 +134,6 @@ function colorCube()
     quad( 4, 5, 6, 7 );
     quad( 5, 4, 0, 1 );
 }
-
 
 window.onload = function init() {
 
@@ -98,38 +147,58 @@ window.onload = function init() {
 
     gl.enable(gl.DEPTH_TEST);
 
-    //
     //  Load shaders and initialize attribute buffers
-    //
     program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
-
     colorCube();
 
+    // Color
     var cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
-
     var vColor = gl.getAttribLocation( program, "vColor" );
     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vColor );
 
+    // Shape
     var vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
-
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
-    thetaLoc = gl.getUniformLocation(program, "theta");
+    // Normal
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
+    var vNormal = gl.getAttribLocation( program, "vNormal" );
+    gl.vertexAttribPointer( vNormal, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vNormal );
 
+    // Light direction
+    var reverseLightDirectionLocation = gl.getUniformLocation( program, "u_reverseLightDirection" );
+    gl.uniform3fv( reverseLightDirectionLocation, [1, 1, 1] );
+
+    //
+    var ambientProduct = mult(lightAmbient, materialAmbient);
+    var diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    var specularProduct = mult(lightSpecular, materialSpecular);
+
+    // Buttons
     document.getElementById("ButtonX").onclick = function(){axis = xAxis;};
     document.getElementById("ButtonY").onclick = function(){axis = yAxis;};
     document.getElementById("ButtonZ").onclick = function(){axis = zAxis;};
     document.getElementById("ButtonT").onclick = function(){flag = !flag;};
-    document.getElementById("ButtonP").onclick = function(){perspective = !perspective;};
+    document.getElementById("ButtonP").onclick = function(){perspec = !perspec;};
     document.getElementById("ButtonC").onclick = function(){direction = !direction;};
+
+    gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"),flatten(ambientProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"),flatten(diffuseProduct) );
+    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"),flatten(specularProduct) );
+    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"),flatten(lightPosition) );
+
+    gl.uniform1f(gl.getUniformLocation(program,"shininess"),materialShininess);
 
     render();
 }
@@ -140,12 +209,7 @@ var render = function() {
       if(direction)
         theta[axis] += 2.0;
       else
-        theta[axis] -= 2.0;
-    }
-    if(perspective)
-      var depth = 1.2;
-    else
-      var depth = 0.0;
+        theta[axis] -= 2.0;}
 
     // Translade slider
     sliderTX = parseFloat(document.getElementById("SliderTX").value);
@@ -154,79 +218,38 @@ var render = function() {
     document.getElementById("ValueTY").innerHTML = sliderTY;
     sliderTZ = parseFloat(document.getElementById("SliderTZ").value);
     document.getElementById("ValueTZ").innerHTML = sliderTZ;
-    n = parseFloat(document.getElementById("SliderPN").value);
-    document.getElementById("ValuePN").innerHTML = n;
-    f = parseFloat(document.getElementById("SliderPF").value);
-    document.getElementById("ValuePF").innerHTML = f;
+    sliderScale = parseFloat(document.getElementById("SliderScale").value);
+    document.getElementById("ValueScale").innerHTML = sliderScale;
+    near = parseFloat(document.getElementById("SliderPN").value);
+    document.getElementById("ValuePN").innerHTML = near;
+    far = parseFloat(document.getElementById("SliderPF").value);
+    document.getElementById("ValuePF").innerHTML = far;
 
-    var theta_x_radians = theta[0] * Math.PI / 180;
-    var s_x = Math.sin( theta_x_radians );
-    var c_x = Math.cos( theta_x_radians );
-    var rx = [
-        1.0,  0.0,  0.0, 0.0,
-        0.0,  c_x,  s_x, 0.0,
-        0.0, -s_x,  c_x, 0.0,
-        0.0,  0.0,  0.0, 1.0];
+    // Light
+    lightX = parseFloat(document.getElementById("LightX").value);
+    document.getElementById("ValueLX").innerHTML = lightX;
+    lightY = parseFloat(document.getElementById("LightY").value);
+    document.getElementById("ValueLY").innerHTML = lightY;
+    lightZ = parseFloat(document.getElementById("LightZ").value);
+    document.getElementById("ValueLZ").innerHTML = lightZ;
+    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"),flatten(vec4(lightX, lightY, lightZ, 0.0 )) );
 
-    var theta_y_radians = theta[1] * Math.PI / 180;
-    var s_y = Math.sin( theta_y_radians );
-    var c_y = Math.cos( theta_y_radians );
-    var ry = [
-        c_y, 0.0, -s_y, 0.0,
-        0.0, 1.0,  0.0, 0.0,
-        s_y, 0.0,  c_y, 0.0,
-        0.0, 0.0,  0.0, 1.0];
+    // Model matrix
+    modelViewMatrix = lookAt(eye, at, up);
+    modelViewMatrix = mult(modelViewMatrix, scalem(sliderScale, sliderScale, sliderScale));
+    modelViewMatrix = mult(modelViewMatrix, translate(sliderTX, sliderTY, sliderTZ));
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta[xAxis], [1, 0, 0]));
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta[yAxis], [0, 1, 0]));
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta[zAxis], [0, 0, 1]));
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelViewMatrix"), false, flatten(modelViewMatrix));
 
-    var theta_z_radians = theta[2] * Math.PI / 180;
-    var s_z = Math.sin( theta_z_radians );
-    var c_z = Math.cos( theta_z_radians );
-    var rz = [
-         c_z, s_z, 0.0, 0.0,
-        -s_z, c_z, 0.0, 0.0,
-         0.0, 0.0, 1.0, 0.0,
-         0.0, 0.0, 0.0, 1.0];
+    // Orthogonal or Perspective
+    if(perspec)
+      projectionMatrix = perspective(fovy, aspect, near, far);
+    else
+      projectionMatrix = ortho(left, right, bottom, ytop, near, far);
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "projectionMatrix"), false, flatten(projectionMatrix));
 
-     var tr = [
-          sliderTZ, 0.0, 0.0, 0.0,
-          0.0, sliderTZ, 0.0, 0.0,
-          0.0, 0.0, sliderTZ, 0.0,
-          sliderTX, sliderTY, 0.0, 1.0];
-
-    var r = 1.0;
-    var l = -1.0;
-    var t = 1.0;
-    var b = -1.0;
-    var proj = [
-         2.0/(r-l), 0.0, 0.0, -(r+l)/(r-l),
-         0.0, 2.0/(t-b), 0.0, -(t+b)/(t-b),
-         0.0, 0.0, -2/(f-n), -(f+n)/(f-n),
-         0.0, 0.0, 0.0, 1.0];
-
-    var depth_mat = [
-        1.0, 0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0,
-        0.0, 0.0, 1.0, -depth,
-        0.0, 0.0, 0.0, 1.0];
-
-    // var matrix = tr * rz * ry * rx;// * proj;
-    // var matrix_loc = gl.getUniformLocation(program, "matrix");
-    // gl.uniformMatrix4fv(matrix_loc, false, matrix); // false means "not transpose"
-
-    var rx_loc = gl.getUniformLocation(program, "rx");
-    var ry_loc = gl.getUniformLocation(program, "ry");
-    var rz_loc = gl.getUniformLocation(program, "rz");
-    var tr_loc = gl.getUniformLocation(program, "tr");
-    var proj_loc = gl.getUniformLocation(program, "proj");
-    var depth_loc = gl.getUniformLocation(program, "depth");
-
-    gl.uniformMatrix4fv(rx_loc, false, rx); // false means "not transpose"
-    gl.uniformMatrix4fv(ry_loc, false, ry); // false means "not transpose"
-    gl.uniformMatrix4fv(rz_loc, false, rz); // false means "not transpose"
-    gl.uniformMatrix4fv(tr_loc, false, tr); // false means "not transpose"
-    gl.uniformMatrix4fv(proj_loc, false, proj); // false means "not transpose"
-    gl.uniformMatrix4fv(depth_loc, false, depth_mat);
-
-    gl.uniform3fv(thetaLoc, theta);
-    gl.drawArrays( gl.TRIANGLES, 0, numVertices );
+    gl.drawArrays(gl.TRIANGLES, 0, numVertices);
     requestAnimFrame(render);
 }
